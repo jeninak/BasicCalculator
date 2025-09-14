a52b988bd93e6715df935288426af037d3187d7e
Jenina <you@example.com>
2025-09-14 16:49:52 +0300

    Added basic operations for the calculator (+, -, *, /).

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            char operation = Console.ReadLine()[0];
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
         }
     }
 }
+
